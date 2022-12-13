b6d0658a4301bedf4358190aeb61e2d3ca37e069
Emma Chirlomez <emma.chirlomez@gmail.com>
2022-12-13 12:36:22 +0100

    fixed the flickering error of the plots in all activities

--- content/psi_helper.py
+++ content/psi_helper.py
@@ -17,177 +17,168 @@ def more_difficult_example(a, b):
 # fig, ax = None, None
 old_figure = None
 
-def show_plots(real_part, imag_part):
-    global old_figure
-    if old_figure is not None:
-        old_figure.close()
-    
-    # if fig is None:
-    fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(11, 7))
-    # old_figure = fig
-    
-    # delete previous graph
-    # for i, j in [(0, 0), (0, 1), (1, 0), (1, 1)]:
-    #     ax[i][j].cla()
-
-    window_size = 5 # TODO: Change
-    fig.tight_layout(pad=3.0)
-    
-    real_window = np.linspace(real_part - window_size, real_part + window_size, 200)
-    imag_window = np.linspace(imag_part - window_size, imag_part + window_size, 200)
-
-    real_f_from_real_changing = np.array([ f(i, imag_part).real for i in real_window])
-    imag_f_from_real_changing = np.array([ f(i, imag_part).imag for i in real_window])
-    real_f_from_imag_changing = np.array([ f(real_part, i).real for i in imag_window])
-    imag_f_from_imag_changing = np.array([ f(real_part, i).imag for i in imag_window])
-    
-    abs_f_from_real_changing = np.array([ np.abs(f(i, imag_part)) for i in real_window])
-    abs_f_from_imag_changing = np.array([ np.abs(f(real_part, i)) for i in imag_window])
-
-    ax[0][0].plot(real_window, real_f_from_real_changing, label='f(X).real')
-    ax[0][0].plot([real_part], [f(real_part, imag_part).real], 'r.', markersize=15)
-    ax[0][0].plot(real_window, imag_f_from_real_changing, label='f(X).imag')
-    ax[0][0].plot([real_part], [f(real_part, imag_part).imag], 'r.', markersize=15)
-    ax[0][0].plot([real_part], [0.], 'g.', markersize=15)
-    ax[0][0].set_xlabel("X.real")
-    ax[0][0].set_title(f"Change in the real part of X, with X.imag={imag_part}")
-    ax[0][0].set_xticks([i + real_part for i in range(-window_size, window_size + 1)])
-
-    ax[0][1].plot(imag_window, real_f_from_imag_changing, label='f(X).real')
-    ax[0][1].plot([imag_part], [f(real_part, imag_part).real], 'r.', markersize=15)
-    ax[0][1].plot(imag_window, imag_f_from_imag_changing, label='f(X).imag')
-    ax[0][1].plot([imag_part], [f(real_part, imag_part).imag], 'r.', markersize=15)
-    ax[0][1].plot([imag_part], [0.], 'g.', markersize=15)
-    ax[0][1].set_xlabel("X.imag")
-    ax[0][1].set_title(f"Change in the imaginary part of X, with X.real={real_part}")
-    ax[0][1].set_xticks([i + imag_part for i in range(-window_size, window_size + 1)])
-
-    ax[1][0].plot(real_window, abs_f_from_real_changing, label='|f(X)|')
-    ax[1][0].plot(real_window, 0 * imag_window, 'g', label='Target |f(X)| = 0')
-    ax[1][0].plot([real_part], [np.abs(f(real_part, imag_part))], 'r.', markersize=15)
-    ax[1][0].set_xlabel("X.real")
-    ax[1][0].set_title(f"Change in the real part of X, with X.imag={imag_part}")
-    ax[1][0].set_xticks([i + real_part for i in range(-window_size, window_size + 1)])
-
-    ax[1][1].plot(imag_window, abs_f_from_imag_changing, label='|f(X)|')
-    ax[1][1].plot(imag_window, 0 * imag_window, 'g', label='Target |f(X)| = 0')
-    ax[1][1].plot([imag_part], [np.abs(f(real_part, imag_part))], 'r.', markersize=15)
-    ax[1][1].set_xlabel("X.imag")
-    ax[1][1].set_title(f"Change in the imaginary part of X, with X.real={real_part}")
-    ax[1][1].set_xticks([i + imag_part for i in range(-window_size, window_size + 1)])
-    
-    for i, j in [(0, 0), (0, 1), (1, 0), (1, 1)]:
-        ax[i][j].grid(alpha=.9, which='both', linestyle='--')
-        ax[i][j].legend()
-        ax[i][j].set_ylim([-5, 5])
-
-    plt.show()
-    return
-
-def show_plots_real_solution(x_value):
-    global old_figure
-    if old_figure is not None:
-        old_figure.close()
-
-    fig, ax = plt.subplots(figsize=(8, 5))
-
-    window_size = 8 # TODO: Change
-    fig.tight_layout(pad=3.0)
-    
-    real_window = np.linspace(x_value - window_size, x_value + window_size, 200)
-
-    real_f_from_real_changing = np.array([ real_solutions_function(i) for i in real_window])
-
-    real_function_zero_value = np.array([0 for i in range(len(real_window))])
-
-    ax.plot(real_window, real_f_from_real_changing, label='f(X)')
-    ax.plot([x_value], [real_solutions_function(x_value)], 'r.', markersize=15)
-    ax.plot(real_window, real_function_zero_value, label='f(X) = 0')
-    ax.plot([x_value], [0.], 'g.', markersize=15)
-    ax.set_xlabel("X")
-    ax.set_ylabel("f(X)")
-    ax.set_title(f"Change in the real part of X")
-    ax.set_xticks([i + x_value for i in range(-window_size, window_size + 1)])
-  
-    ax.grid(alpha=.9, which='both', linestyle='--')
-    ax.legend()
-    ax.set_ylim([-20, 20])
-    
-    plt.show()
-    return
-
-def show_more_difficult_example(real_part, imag_part):
-    global old_figure
-    if old_figure is not None:
-        old_figure.close()
-        
-    fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(11, 7))
-
-    window_size = 5 # TODO: Change
-    fig.tight_layout(pad=3.0)
-    
-    real_window = np.linspace(real_part - window_size, real_part + window_size, 200)
-    imag_window = np.linspace(imag_part - window_size, imag_part + window_size, 200)
-
-    real_f_from_real_changing = np.array([ more_difficult_example(i, imag_part).real for i in real_window])
-    imag_f_from_real_changing = np.array([ more_difficult_example(i, imag_part).imag for i in real_window])
-    real_f_from_imag_changing = np.array([ more_difficult_example(real_part, i).real for i in imag_window])
-    imag_f_from_imag_changing = np.array([ more_difficult_example(real_part, i).imag for i in imag_window])
-    
-    abs_f_from_real_changing = np.array([ np.abs(more_difficult_example(i, imag_part)) for i in real_window])
-    abs_f_from_imag_changing = np.array([ np.abs(more_difficult_example(real_part, i)) for i in imag_window])
-
-    ax[0][0].plot(real_window, real_f_from_real_changing, label='f(X).real')
-    ax[0][0].plot([real_part], [more_difficult_example(real_part, imag_part).real], 'r.', markersize=15)
-    ax[0][0].plot(real_window, imag_f_from_real_changing, label='f(X).imag')
-    ax[0][0].plot([real_part], [more_difficult_example(real_part, imag_part).imag], 'r.', markersize=15)
-    ax[0][0].plot([real_part], [0.], 'g.', markersize=15)
-    ax[0][0].set_xlabel("X.real")
-    ax[0][0].set_title(f"Change in the real part of X, with X.imag={imag_part}")
-    ax[0][0].set_xticks([i + real_part for i in range(-window_size, window_size + 1)])
-
-    ax[0][1].plot(imag_window, real_f_from_imag_changing, label='f(X).real')
-    ax[0][1].plot([imag_part], [more_difficult_example(real_part, imag_part).real], 'r.', markersize=15)
-    ax[0][1].plot(imag_window, imag_f_from_imag_changing, label='f(X).imag')
-    ax[0][1].plot([imag_part], [more_difficult_example(real_part, imag_part).imag], 'r.', markersize=15)
-    ax[0][1].plot([imag_part], [0.], 'g.', markersize=15)
-    ax[0][1].set_xlabel("X.imag")
-    ax[0][1].set_title(f"Change in the imaginary part of X, with X.real={real_part}")
-    ax[0][1].set_xticks([i + imag_part for i in range(-window_size, window_size + 1)])
-
-    ax[1][0].plot(real_window, abs_f_from_real_changing, label='|f(X)|')
-    ax[1][0].plot(real_window, 0 * imag_window, 'g', label='Target |f(X)| = 0')
-    ax[1][0].plot([real_part], [np.abs(more_difficult_example(real_part, imag_part))], 'r.', markersize=15)
-    ax[1][0].set_xlabel("X.real")
-    ax[1][0].set_title(f"Change in the real part of X, with X.imag={imag_part}")
-    ax[1][0].set_xticks([i + real_part for i in range(-window_size, window_size + 1)])
-
-    ax[1][1].plot(imag_window, abs_f_from_imag_changing, label='|f(X)|')
-    ax[1][1].plot(imag_window, 0 * imag_window, 'g', label='Target |f(X)| = 0')
-    ax[1][1].plot([imag_part], [np.abs(more_difficult_example(real_part, imag_part))], 'r.', markersize=15)
-    ax[1][1].set_xlabel("X.imag")
-    ax[1][1].set_title(f"Change in the imaginary part of X, with X.real={real_part}")
-    ax[1][1].set_xticks([i + imag_part for i in range(-window_size, window_size + 1)])
-    
-    for i, j in [(0, 0), (0, 1), (1, 0), (1, 1)]:
-        ax[i][j].grid(alpha=.9, which='both', linestyle='--')
-        ax[i][j].legend()
-        #ax[i][j].set_ylim([-5, 5])
-
-    plt.show()
-    return
-
 def exercise_1():
+    fig, ax = plt.subplots(figsize=(8, 5))
+    
+    def show_plots_real_solution(x_value):
+
+        ax.cla()
+
+        window_size = 8 # TODO: Change
+        fig.tight_layout(pad=3.0)
+
+        real_window = np.linspace(x_value - window_size, x_value + window_size, 200)
+
+        real_f_from_real_changing = np.array([ real_solutions_function(i) for i in real_window])
+
+        real_function_zero_value = np.array([0 for i in range(len(real_window))])
+
+        ax.plot(real_window, real_f_from_real_changing, label='f(X)')
+        ax.plot([x_value], [real_solutions_function(x_value)], 'r.', markersize=15)
+        ax.plot(real_window, real_function_zero_value, label='f(X) = 0')
+        ax.plot([x_value], [0.], 'g.', markersize=15)
+        ax.set_xlabel("X")
+        ax.set_ylabel("f(X)")
+        ax.set_title(f"Change in the real part of X")
+        ax.set_xticks([i + x_value for i in range(-window_size, window_size + 1)])
+
+        ax.grid(alpha=.9, which='both', linestyle='--')
+        ax.legend()
+        ax.set_ylim([-20, 20])
+
+        # plt.show()
+        return None
+    
     x = widgets.IntSlider(min=-10, max=10, step=1, value=0)
+    
     interact(show_plots_real_solution, x_value=x);
 
 def exercise_2():
+    fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(11, 7))
+    
+    def show_plots(real_part, imag_part):
+        
+        # delete previous graph
+        for i, j in [(0, 0), (0, 1), (1, 0), (1, 1)]:
+            ax[i][j].cla()
+
+        window_size = 5 # TODO: Change
+        fig.tight_layout(pad=3.0)
+
+        real_window = np.linspace(real_part - window_size, real_part + window_size, 200)
+        imag_window = np.linspace(imag_part - window_size, imag_part + window_size, 200)
+
+        real_f_from_real_changing = np.array([ f(i, imag_part).real for i in real_window])
+        imag_f_from_real_changing = np.array([ f(i, imag_part).imag for i in real_window])
+        real_f_from_imag_changing = np.array([ f(real_part, i).real for i in imag_window])
+        imag_f_from_imag_changing = np.array([ f(real_part, i).imag for i in imag_window])
+
+        abs_f_from_real_changing = np.array([ np.abs(f(i, imag_part)) for i in real_window])
+        abs_f_from_imag_changing = np.array([ np.abs(f(real_part, i)) for i in imag_window])
+
+        ax[0][0].plot(real_window, real_f_from_real_changing, label='f(X).real')
+        ax[0][0].plot([real_part], [f(real_part, imag_part).real], 'r.', markersize=15)
+        ax[0][0].plot(real_window, imag_f_from_real_changing, label='f(X).imag')
+        ax[0][0].plot([real_part], [f(real_part, imag_part).imag], 'r.', markersize=15)
+        ax[0][0].plot([real_part], [0.], 'g.', markersize=15)
+        ax[0][0].set_xlabel("X.real")
+        ax[0][0].set_title(f"Change in the real part of X, with X.imag={imag_part}")
+        ax[0][0].set_xticks([i + real_part for i in range(-window_size, window_size + 1)])
+
+        ax[0][1].plot(imag_window, real_f_from_imag_changing, label='f(X).real')
+        ax[0][1].plot([imag_part], [f(real_part, imag_part).real], 'r.', markersize=15)
+        ax[0][1].plot(imag_window, imag_f_from_imag_changing, label='f(X).imag')
+        ax[0][1].plot([imag_part], [f(real_part, imag_part).imag], 'r.', markersize=15)
+        ax[0][1].plot([imag_part], [0.], 'g.', markersize=15)
+        ax[0][1].set_xlabel("X.imag")
+        ax[0][1].set_title(f"Change in the imaginary part of X, with X.real={real_part}")
+        ax[0][1].set_xticks([i + imag_part for i in range(-window_size, window_size + 1)])
+
+        ax[1][0].plot(real_window, abs_f_from_real_changing, label='|f(X)|')
+        ax[1][0].plot(real_window, 0 * imag_window, 'g', label='Target |f(X)| = 0')
+        ax[1][0].plot([real_part], [np.abs(f(real_part, imag_part))], 'r.', markersize=15)
+        ax[1][0].set_xlabel("X.real")
+        ax[1][0].set_title(f"Change in the real part of X, with X.imag={imag_part}")
+        ax[1][0].set_xticks([i + real_part for i in range(-window_size, window_size + 1)])
+
+        ax[1][1].plot(imag_window, abs_f_from_imag_changing, label='|f(X)|')
+        ax[1][1].plot(imag_window, 0 * imag_window, 'g', label='Target |f(X)| = 0')
+        ax[1][1].plot([imag_part], [np.abs(f(real_part, imag_part))], 'r.', markersize=15)
+        ax[1][1].set_xlabel("X.imag")
+        ax[1][1].set_title(f"Change in the imaginary part of X, with X.real={real_part}")
+        ax[1][1].set_xticks([i + imag_part for i in range(-window_size, window_size + 1)])
+
+        for i, j in [(0, 0), (0, 1), (1, 0), (1, 1)]:
+            ax[i][j].grid(alpha=.9, which='both', linestyle='--')
+            ax[i][j].legend()
+            ax[i][j].set_ylim([-5, 5])
+
+        return None
     real_part = widgets.IntSlider(min=-10, max=10, step=1, value=0)
     imag_part = widgets.IntSlider(min=-10, max=10, step=1, value=0)
 
     interact(show_plots, real_part=real_part, imag_part=imag_part);
 
 def exercise_3():
+    fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(11, 7))
+    
+    def show_more_difficult_example(real_part, imag_part):
+        
+        for i, j in [(0, 0), (0, 1), (1, 0), (1, 1)]:
+            ax[i][j].cla()
+
+        window_size = 5 # TODO: Change
+        fig.tight_layout(pad=3.0)
+
+        real_window = np.linspace(real_part - window_size, real_part + window_size, 200)
+        imag_window = np.linspace(imag_part - window_size, imag_part + window_size, 200)
+
+        real_f_from_real_changing = np.array([ more_difficult_example(i, imag_part).real for i in real_window])
+        imag_f_from_real_changing = np.array([ more_difficult_example(i, imag_part).imag for i in real_window])
+        real_f_from_imag_changing = np.array([ more_difficult_example(real_part, i).real for i in imag_window])
+        imag_f_from_imag_changing = np.array([ more_difficult_example(real_part, i).imag for i in imag_window])
+
+        abs_f_from_real_changing = np.array([ np.abs(more_difficult_example(i, imag_part)) for i in real_window])
+        abs_f_from_imag_changing = np.array([ np.abs(more_difficult_example(real_part, i)) for i in imag_window])
+
+        ax[0][0].plot(real_window, real_f_from_real_changing, label='f(X).real')
+        ax[0][0].plot([real_part], [more_difficult_example(real_part, imag_part).real], 'r.', markersize=15)
+        ax[0][0].plot(real_window, imag_f_from_real_changing, label='f(X).imag')
+        ax[0][0].plot([real_part], [more_difficult_example(real_part, imag_part).imag], 'r.', markersize=15)
+        ax[0][0].plot([real_part], [0.], 'g.', markersize=15)
+        ax[0][0].set_xlabel("X.real")
+        ax[0][0].set_title(f"Change in the real part of X, with X.imag={imag_part}")
+        ax[0][0].set_xticks([i + real_part for i in range(-window_size, window_size + 1)])
+
+        ax[0][1].plot(imag_window, real_f_from_imag_changing, label='f(X).real')
+        ax[0][1].plot([imag_part], [more_difficult_example(real_part, imag_part).real], 'r.', markersize=15)
+        ax[0][1].plot(imag_window, imag_f_from_imag_changing, label='f(X).imag')
+        ax[0][1].plot([imag_part], [more_difficult_example(real_part, imag_part).imag], 'r.', markersize=15)
+        ax[0][1].plot([imag_part], [0.], 'g.', markersize=15)
+        ax[0][1].set_xlabel("X.imag")
+        ax[0][1].set_title(f"Change in the imaginary part of X, with X.real={real_part}")
+        ax[0][1].set_xticks([i + imag_part for i in range(-window_size, window_size + 1)])
+
+        ax[1][0].plot(real_window, abs_f_from_real_changing, label='|f(X)|')
+        ax[1][0].plot(real_window, 0 * imag_window, 'g', label='Target |f(X)| = 0')
+        ax[1][0].plot([real_part], [np.abs(more_difficult_example(real_part, imag_part))], 'r.', markersize=15)
+        ax[1][0].set_xlabel("X.real")
+        ax[1][0].set_title(f"Change in the real part of X, with X.imag={imag_part}")
+        ax[1][0].set_xticks([i + real_part for i in range(-window_size, window_size + 1)])
+
+        ax[1][1].plot(imag_window, abs_f_from_imag_changing, label='|f(X)|')
+        ax[1][1].plot(imag_window, 0 * imag_window, 'g', label='Target |f(X)| = 0')
+        ax[1][1].plot([imag_part], [np.abs(more_difficult_example(real_part, imag_part))], 'r.', markersize=15)
+        ax[1][1].set_xlabel("X.imag")
+        ax[1][1].set_title(f"Change in the imaginary part of X, with X.real={real_part}")
+        ax[1][1].set_xticks([i + imag_part for i in range(-window_size, window_size + 1)])
+
+        for i, j in [(0, 0), (0, 1), (1, 0), (1, 1)]:
+            ax[i][j].grid(alpha=.9, which='both', linestyle='--')
+            ax[i][j].legend()
+            #ax[i][j].set_ylim([-5, 5])
+
+        return None
     real_part = widgets.IntSlider(min=-10, max=10, step=1, value=0)
     imag_part = widgets.IntSlider(min=-10, max=10, step=1, value=0)
 
